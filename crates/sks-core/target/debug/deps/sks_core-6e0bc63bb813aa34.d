a067d02f21d6f8b3de0fa63b8d6be424ae55b942
cdw0424 <cdw0424@gmail.com>
2026-04-25 20:04:11 +0900

    Remove tracked Rust build artifacts

--- crates/sks-core/target/debug/deps/sks_core-6e0bc63bb813aa34.d
+++
@@ -1,5 +0,0 @@
-/Users/weklem/Desktop/devs/DCodex/crates/sks-core/target/debug/deps/sks_core-6e0bc63bb813aa34.d: src/main.rs
-
-/Users/weklem/Desktop/devs/DCodex/crates/sks-core/target/debug/deps/libsks_core-6e0bc63bb813aa34.rmeta: src/main.rs
-
-src/main.rs:
